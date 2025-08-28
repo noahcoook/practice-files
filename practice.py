6b04fd8b67cbdfe79c3b0c3e6258d620e849423b
noahcoook <cooknoah04@gmail.com>
2025-08-28 14:08:10 -0700

    quick age asker

---
+++ practice.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 21:
+	print ('you are old enough to drink in the US')
+elif age >= 18:
+	print('you can drink in the UK')
+elif age >= 16:
+	print('you can drive')
+else:
+	print('you are too young to do anything')
